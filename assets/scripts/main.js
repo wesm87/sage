7be80acc5ee09d7159e57d5025ff19ee349d062d
Wes Moberly <wesm87@gmail.com>
2016-03-23 02:53:40 -0400

    First pass at refactoring
    - Reformatted code to follow WordPress standards (PHP, SCSS, JS)
    - Translated strings are now escaped properly
    - Added documentation for most of the code
    - Fixed `.editorconfig`
    - Added `.scss-lint.yml`

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,66 +12,70 @@
 
 (function($) {
 
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var Sage = {
-    // All pages
-    'common': {
-      init: function() {
-        // JavaScript to be fired on all pages
-      },
-      finalize: function() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
-      }
-    },
-    // Home page
-    'home': {
-      init: function() {
-        // JavaScript to be fired on the home page
-      },
-      finalize: function() {
-        // JavaScript to be fired on the home page, after the init JS
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
-      init: function() {
-        // JavaScript to be fired on the about us page
-      }
-    }
-  };
+	// Use this variable to set up the common and page specific functions. If you
+	// rename this variable, you will also need to rename the namespace below.
+	var Sage = {
 
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var fire;
-      var namespace = Sage;
-      funcname = (funcname === undefined) ? 'init' : funcname;
-      fire = func !== '';
-      fire = fire && namespace[func];
-      fire = fire && typeof namespace[func][funcname] === 'function';
+		// All pages
+		'common': {
+			init: function() {
+				// JavaScript to be fired on all pages
+			},
+			finalize: function() {
+				// JavaScript to be fired on all pages, after page specific JS is fired
+			}
+		},
 
-      if (fire) {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      // Fire common init JS
-      UTIL.fire('common');
+		// Home page
+		'home': {
+			init: function() {
+				// JavaScript to be fired on the home page
+			},
+			finalize: function() {
+				// JavaScript to be fired on the home page, after the init JS
+			}
+		},
 
-      // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-        UTIL.fire(classnm, 'finalize');
-      });
+		// About us page, note the change from about-us to about_us.
+		'about_us': {
+			init: function() {
+				// JavaScript to be fired on the about us page
+			}
+		}
+	};
 
-      // Fire common finalize JS
-      UTIL.fire('common', 'finalize');
-    }
-  };
+	// The routing fires all common scripts, followed by the page specific scripts.
+	// Add additional events for more control over timing e.g. a finalize event
+	var UTIL = {
+		fire: function( func, funcname, args ) {
+			var fire;
+			var namespace = Sage;
+			funcname = ( funcname === undefined ) ? 'init' : funcname;
+			fire = func !== '';
+			fire = fire && namespace[func];
+			fire = fire && typeof namespace[func][funcname] === 'function';
 
-  // Load Events
-  $(document).ready(UTIL.loadEvents);
+			if ( fire ) {
+				namespace[func][funcname](args);
+			}
+		},
+		loadEvents: function() {
+			// Fire common init JS
+			UTIL.fire( 'common' );
+
+			// Fire page-specific init JS, and then finalize JS
+			var pages = document.body.className.replace( /-/g, '_' ).split( /\s+/ );
+			$.each( pages, function( i, classnm ) {
+				UTIL.fire( classnm );
+				UTIL.fire( classnm, 'finalize' );
+			});
+
+			// Fire common finalize JS
+			UTIL.fire('common', 'finalize');
+		}
+	};
+
+	// Load Events
+	$(document).ready(UTIL.loadEvents);
 
 })(jQuery); // Fully reference jQuery after this point.
--- lib/wrapper.php
+++ lib/wrapper.php
@@ -3,62 +3,78 @@
 namespace Roots\Sage\Wrapper;
 
 /**
- * Theme wrapper
+ * Theme wrapper.
  *
  * @link https://roots.io/sage/docs/theme-wrapper/
  * @link http://scribu.net/wordpress/theme-wrappers.html
  */
-
 function template_path() {
-  return SageWrapping::$main_template;
+	return SageWrapping::$main_template;
 }
 
 function sidebar_path() {
-  return new SageWrapping('templates/sidebar.php');
+	return new SageWrapping( 'templates/sidebar.php' );
 }
 
 class SageWrapping {
-  // Stores the full path to the main template file
-  public static $main_template;
 
-  // Basename of template file
-  public $slug;
+	/**
+	 * Stores the full path to the main template file.
+	 *
+	 * @var string
+	 */
+	public static $main_template;
 
-  // Array of templates
-  public $templates;
+	/**
+	 * Basename of template file.
+	 *
+	 * @var string
+	 */
+	public $slug;
 
-  // Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
-  public static $base;
+	/**
+	 * Array of templates.
+	 *
+	 * @var array
+	 */
+	public $templates;
 
-  public function __construct($template = 'base.php') {
-    $this->slug = basename($template, '.php');
-    $this->templates = [$template];
+	/**
+	 * Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+	 *
+	 * @var string
+	 */
+	public static $base;
 
-    if (self::$base) {
-      $str = substr($template, 0, -4);
-      array_unshift($this->templates, sprintf($str . '-%s.php', self::$base));
-    }
-  }
+	public function __construct( $template = 'base.php' ) {
+		$this->slug = basename( $template, '.php' );
+		$this->templates = [ $template ];
 
-  public function __toString() {
-    $this->templates = apply_filters('sage/wrap_' . $this->slug, $this->templates);
-    return locate_template($this->templates);
-  }
+		if ( self::$base ) {
+			$str = substr( $template, 0, -4 );
+			array_unshift( $this->templates, sprintf( $str . '-%s.php', self::$base ) );
+		}
+	}
 
-  public static function wrap($main) {
-    // Check for other filters returning null
-    if (!is_string($main)) {
-      return $main;
-    }
+	public function __toString() {
+		$this->templates = apply_filters( 'sage/wrap_' . $this->slug, $this->templates );
+		return locate_template( $this->templates );
+	}
 
-    self::$main_template = $main;
-    self::$base = basename(self::$main_template, '.php');
+	public static function wrap( $main ) {
+		// Check for other filters returning null.
+		if ( ! is_string( $main ) ) {
+			return $main;
+		}
 
-    if (self::$base === 'index') {
-      self::$base = false;
-    }
+		self::$main_template = $main;
+		self::$base = basename( self::$main_template, '.php' );
 
-    return new SageWrapping();
-  }
+		if ( 'index' === self::$base ) {
+			self::$base = false;
+		}
+
+		return new SageWrapping();
+	}
 }
-add_filter('template_include', [__NAMESPACE__ . '\\SageWrapping', 'wrap'], 109);
+add_filter( 'template_include', [ __NAMESPACE__ . '\\SageWrapping', 'wrap' ], 109 );
